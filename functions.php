c0b89e8b0d464ee90d4a0f293fedc07d461ab9a4
wpsoul <sizamtheme@gmail.com>
2025-07-27 00:33:04 +0200

    update

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  */
 
 if ( !defined( 'GREENLIGHT_THEME_VERSION' ) ) {
-	define('GREENLIGHT_THEME_VERSION', '1.2');
+	define('GREENLIGHT_THEME_VERSION', '1.3');
 }
 if ( !defined( 'GREENLIGHT_THEME_DIR' ) ) {
 	define('GREENLIGHT_THEME_DIR', get_template_directory_uri());
